b57da21ed40a1680e6764b49853490b03102d904
tluigimm <tluigimm@tutanota.com>
2020-10-24 19:21:38 -0300

    connected to NASA api and showing image+discription+title

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
 const mysql = require("mysql");
 const fetch = require("node-fetch");
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const API_KEY = "Y4puG7EP9MzY7JpYeOZKMTTJlzX5DMuSQdEbkL8r";
 
@@ -12,32 +16,47 @@ const conn = mysql.createConnection({
 });
 const app = express();
 
-//app.get("/", (req, res) => {
-//   conn.query('SELECT * FROM user', (err, results, fields) => {
-//        if (err) throw err;
-//        res.json(results);
-//    });
-//});
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/fetch_image', async (req, res) => {
-    const url = `https://api.nasa.gov/planetary/apod?date=2020-10-18&api_key=${API_KEY}`;
-    const options = {
-        "method": "GET",
-    };
-    const response = await fetch(url, options)
-        .then(res => res.json())
-        .catch(e => {
-          console.error({
-            "message": ":(",
-             error: e,
-          });
-        });
-    console.log("RESPONSE: ", response);
-    res.json(response);
+app.get("/login", (req, res) => {
+    const u = req.body.u; 
+    const p = req.body.p;
+    conn.query(`SELECT * FROM user WHERE (name,pswd)=(${u},${p});`, (err, results, fields) => {
+         if (err) throw err;
+         res.json(results);
+    });
+});
+
+app.post('/send_date', async (req, res) => {
+    try{
+        const d = req.body.d;
+        const url = `https://api.nasa.gov/planetary/apod?date=${d}&api_key=${API_KEY}`;
+        const options = {
+            "method": "GET",
+        };
+
+        const respond = await fetch(url, options)
+            .then(res => res.json())
+            .catch(e => {
+              console.error({
+                "message": ":(",
+                 error: e,
+              });
+            });
+        console.log("RESPONSE: ", response)
+        res.json(res);
+    } catch (err) {
+        console.log(err);
+    }
+
 });
 
 
-app.listen(7000, () => {
-        console.log("running on port 7000!");
+app.listen(7002, async () => {
+        console.log("running on port 7002!");
 });
 
